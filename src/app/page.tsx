bd69c355f8e795e4e4831d204f355341395790d6
lapos <lapos@aconsulting.sk>
2023-06-19 13:20:42 +0200

    Pridal som video na homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image"
+import VideoPlayer from "./Home_Modules/VidePlayer"
 
 export default function Home() {
 
   return (
     <>
+      <VideoPlayer />
       <div className="flex justify-center">
-        <h1 className="">Home</h1>
+        <h1>Hello World</h1>
+  
       </div>
     </>
   )
