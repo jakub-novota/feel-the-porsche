e0a208635eacdd0b95b87b25123b0cf678cb0307
lapos <lapos@aconsulting.sk>
2023-07-07 12:06:07 +0200

    pridal som aby sa dalo kliknut na obrazky a zvečil obrazok

--- src/app/test/admin/[id]/modules/groups/ImagesAndGallery.tsx
+++ src/app/test/admin/[id]/modules/groups/ImagesAndGallery.tsx
@@ -20,6 +20,17 @@ export default function ImagesAndGallery({ car }: ImagesAndGalleryProps): JSX.El
         document.body.style.overflow = 'auto';
     };
 
+    const getImageContainerStyle = () => {
+        if (enlargedImage) {
+            return {
+                cursor: 'default',
+            };
+        }
+        return {
+            cursor: 'pointer',
+        };
+    };
+
     return (
         <div className="border border-gray-300 rounded p-4">
             <h2 className="text-lg font-semibold mb-2">Details and Description</h2>
@@ -27,26 +38,25 @@ export default function ImagesAndGallery({ car }: ImagesAndGalleryProps): JSX.El
                 <h2 className="text-lg font-semibold mb-2">Images and Gallery</h2>
                 <div className="mb-4">
                     <h3 className="text-md font-semibold mb-2">Frontpage Image</h3>
-                    <div className="w-24 h-24 relative">
-                        <img
-                            src={car.image}
-                            alt="Frontpage Image"
-                            className="w-full h-full object-cover rounded cursor-pointer"
-                            onClick={() => handleImageClick(car.image)}
-                        />
+                    <div
+                        className="w-24 h-24 relative"
+                        style={getImageContainerStyle()}
+                        onClick={() => handleImageClick(car.image)}
+                    >
+                        <img src={car.image} alt="Frontpage Image" className="w-full h-full object-cover rounded" />
                     </div>
                 </div>
                 <div className="mb-4">
                     <h3 className="text-md font-semibold mb-2">Cover Images</h3>
                     <div className="flex flex-wrap">
                         {Object.entries(car.image_cars).map(([key, image]) => (
-                            <div key={key} className="w-24 h-24 m-2 relative">
-                                <img
-                                    src={image}
-                                    alt={`Cover Image ${key}`}
-                                    className="w-full h-full object-cover rounded cursor-pointer"
-                                    onClick={() => handleImageClick(image)}
-                                />
+                            <div
+                                key={key}
+                                className="w-24 h-24 m-2 relative"
+                                style={getImageContainerStyle()}
+                                onClick={() => handleImageClick(image)}
+                            >
+                                <img src={image} alt={`Cover Image ${key}`} className="w-full h-full object-cover rounded" />
                             </div>
                         ))}
                     </div>
@@ -55,13 +65,13 @@ export default function ImagesAndGallery({ car }: ImagesAndGalleryProps): JSX.El
                     <h3 className="text-md font-semibold mb-2">Gallery Images</h3>
                     <div className="flex flex-wrap">
                         {Object.entries(car.gallery).map(([key, image]) => (
-                            <div key={key} className="w-24 h-24 m-2 relative">
-                                <img
-                                    src={image}
-                                    alt={`Gallery Image ${key}`}
-                                    className="w-full h-full object-cover rounded cursor-pointer"
-                                    onClick={() => handleImageClick(image)}
-                                />
+                            <div
+                                key={key}
+                                className="w-24 h-24 m-2 relative"
+                                style={getImageContainerStyle()}
+                                onClick={() => handleImageClick(image)}
+                            >
+                                <img src={image} alt={`Gallery Image ${key}`} className="w-full h-full object-cover rounded" />
                             </div>
                         ))}
                     </div>
@@ -71,7 +81,7 @@ export default function ImagesAndGallery({ car }: ImagesAndGalleryProps): JSX.El
                         className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-black bg-opacity-50 z-50"
                         onClick={handleCloseImage}
                     >
-                        <img src={enlargedImage} alt="Enlarged Image" className="max-w-full max-h-full cursor-pointer" />
+                        <img src={enlargedImage} alt="Enlarged Image" className="max-w-full max-h-full" />
                     </div>
                 )}
             </div>
