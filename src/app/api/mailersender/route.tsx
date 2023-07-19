4ba7a6cedbe32493e9e022a5a1e74b8b8666f92e
lapos <lapos@aconsulting.sk>
2023-07-19 16:06:28 +0200

    vercel

--- src/app/api/mailersender/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { SendEmail } from '@/app/Modules/MailerSender/SendMail'
-
-export async function POST(req: any, res: any) {
-    const data = await req.json();
-    console.log("Recived", data)
-    SendEmail(data.name, data.email, data.message);
-    return NextResponse.json("Email sent");
-}
